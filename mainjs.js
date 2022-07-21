fe69722d42174db8555a6f130f2f842a7ed364fa
andrescastro100 <castroauteriandres@gmail.com>
2022-07-20 23:48:44 -0300

    primer pre entrega

--- mainjs.js
+++ mainjs.js
@@ -4,66 +4,97 @@
 }
 */
 class User {
-    constructor(nombre, edad, num) {
+    constructor(nombre, edad, num,cantidad,total) {
         this.nombre = nombre;
         this.edad = edad;
         this.num = num;
+        this.cantidad = cantidad;
+        this.total = total;
     }
 }
-let nomUser = prompt("Ingrese su nombre: ");
-let edadUser = prompt("Ingrese edad: ");
-let numUser = prompt("ingrese un numero de contacto");
+usuarios=[];
 
-const User1 = new User(nomUser.toUpperCase(),edadUser,numUser);
-console.log(User1);
-let bandera=1,calc=0;
-const vino=250, vodka=300, fernet=500, cerveza=200, gin=500;   
+let bandera=1,calc=0,bandera1=1,bandera2=1;
+const bebidas=[vino=250, vodka=300, fernet=500, gin=500, cerveza=200]; 
 
-if ( User1.edad >= 18){
+
+
     while (bandera==1){
-        select(vino, vodka, fernet, cerveza, gin);
+        select(bebidas);
     }
-}
-
-else{
-    alert("Hola " + User1.nombre + " te informamos que no cumples con la edad minima requerida");
-}
 
 function select (){
-    let = cantidadvino=0,cantidadvodka=0,cantidadfernet=0,cantidadgin=0,cantidadcerveza=0;
-    let opcion= prompt("Ingrese su bebida \n 1) vino: "+vino+"\n  2) vodka: "+vodka+"\n 3) fernet: "+fernet+"\n 4) gin: "+gin+"\n 5) cerveza: "+ cerveza);
-
-    switch (opcion){
-        case '1': 
-            cantidadvino=prompt("ingrese la cantidad de copas de Vino $" + vino+" c/u");
-            calc=calc+(vino*cantidadvino);
+    
+    while (bandera1==1){
+    let cantidadBe= [cantidadvino=0,cantidadvodka=0,cantidadfernet=0,cantidadgin=0,cantidadcerveza=0];
+    let nomUser = prompt("Ingrese su nombre: ");
+    let edadUser = prompt("Ingrese edad: ");
+    let numUser = prompt("ingrese un numero de contacto");
+    let carrito = []; 
+    if ( edadUser >= 18){
+        while (bandera2==1){
+            let opcion= prompt("Ingrese su bebida \n 1) vino: "+bebidas[0]+"\n  2) vodka: "+bebidas[1]+"\n 3) fernet: "+bebidas[2]+"\n 4) gin: "+bebidas[3]+"\n 5) cerveza: "+ bebidas[4]);
+            switch (opcion){
+            
+            case '1': 
+            cantidadBe[0]=prompt("ingrese la cantidad de copas de Vino $" + bebidas[0]+" c/u");
+            if (cantidadBe[0]!=0){
+            carrito.push(bebidas[0]*cantidadBe[0]);
+            }
             break;
-        case '2': 
-            cantidadvodka=prompt("ingrese la cantidad de vasos de Vodka $"+ vodka+" c/u");
-            calc=calc+(vodka*cantidadvodka);
+            
+            case '2': 
+            cantidadBe[1]=prompt("ingrese la cantidad de vasos de Vodka $"+ bebidas[1]+" c/u");
+            if (cantidadBe[1] != 0){
+            carrito.push(bebidas[1]*cantidadBe[1]);
+            }
             break
-        case '3': 
-            cantidadfernet=prompt("ingrese la cantidad de vasos de Fernet $" + fernet+" c/u");
-            calc=calc+(fernet*cantidadfernet);
+            
+            case '3': 
+            cantidadBe[2]=prompt("ingrese la cantidad de vasos de Vodka $"+ bebidas[2]+" c/u");
+            if (cantidadBe[2] != 0){
+            carrito.push(bebidas[2]*cantidadBe[2]);
+            }
             break;
         
-        case '4':
-            cantidadgin=prompt("ingrese la cantidad de copas de Gin $" + gin+ " c/u");
-            calc=calc+(gin*cantidadgin);
+            
+            case '4':
+            cantidadBe[3]=prompt("ingrese la cantidad de vasos de Vodka $"+ bebidas[3]+" c/u");
+            if (cantidadBe[3] != 0){
+            carrito.push(bebidas[3]*cantidadBe[3]);
+            }
             break;
         
-        case '5': 
-            cantidadcerveza=prompt("ingrese la cantidad de latas de Cerveza $" + cerveza+ " c/u");
-            calc=calc+(cerveza*cantidadcerveza);
+            
+            case '5': 
+            cantidadBe[4]=prompt("ingrese la cantidad de vasos de Vodka $"+ bebidas[4]+" c/u");
+            if (cantidadBe[4] != 0){
+            carrito.push(bebidas[4]*cantidadBe[4]);
+            }
             break;
-    }
-    bandera=prompt("¿Quiere seleccionar otra bebida? \n precisione 1 si desea continuar y 0 si desea terminar");
-        if(bandera==0){
-            alert("Gracias por su compra su monto es $"+calc);
-        }
-        else if(bandera !=1 && bandera !=0){
+            }
+            bandera2 = prompt("¿Quiere seleccionar otra bebida? \n precisione 1 si desea continuar y 0 si desea terminar");
+            if(bandera2== 0){
+            for(var i=0; i<carrito.length; i++){ 
+                calc += carrito[i];
+            }
+            usuarios.push(new User(nomUser.toUpperCase(),edadUser,numUser,cantidadBe,calc));
+            console.log(usuarios); 
+            }
+            else if(bandera2 !=1 && bandera2 !=0){
             alert("Error");
+            }
         }
+    }
+    
+    else{
+        alert("Hola " + nomUser + " te informamos que no cumples con la edad minima requerida");
+    }
+    bandera1 = prompt("Desea ingresar un nuevo pedido \n 1 en caso de querer agregar \n 0 en caso de no querer agregar mas")
+    if (bandera1 == 0){
+        bandera=0;
+    }
+    }
 }
 
 
