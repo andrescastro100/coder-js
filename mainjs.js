f10a15e6682837b17ea58756a75a900244fd5a64
andrescastro100 <castroauteriandres@gmail.com>
2022-07-27 20:44:23 -0300

    primera preentrega correccion

--- mainjs.js
+++ mainjs.js
@@ -4,98 +4,62 @@
 }
 */
 class User {
-    constructor(nombre, edad, num,cantidad,total) {
+
+    constructor(nombre, edad) {
         this.nombre = nombre;
         this.edad = edad;
-        this.num = num;
-        this.cantidad = cantidad;
-        this.total = total;
     }
 }
-usuarios=[];
+class Articulo{
 
-let bandera=1,calc=0,bandera1=1,bandera2=1;
-const bebidas=[vino=250, vodka=300, fernet=500, gin=500, cerveza=200]; 
-
-
-
-    while (bandera==1){
-        select(bebidas);
+	constructor(descripcion, precio){
+		this.descripcion = descripcion;
+		this.precio = precio;   
+	}
+    asignarId(array){
+        this.id = array.length;
     }
+}
 
-function select (){
-    
-    while (bandera1==1){
-    let cantidadBe= [cantidadvino=0,cantidadvodka=0,cantidadfernet=0,cantidadgin=0,cantidadcerveza=0];
-    let nomUser = prompt("Ingrese su nombre: ");
-    let edadUser = prompt("Ingrese edad: ");
-    let numUser = prompt("ingrese un numero de contacto");
-    let carrito = []; 
+const articulo = [
+    new Articulo("Vino","500"),
+    new Articulo("Cerveza","300"),
+    new Articulo("Fernet","700"),
+    new Articulo("Vodka","700"),
+    new Articulo("Gin","750") 
+]
+
+
+let nomUser= prompt("Ingrese su Nombre: ");
+let edadUser= prompt("Ingrese su Edad: ");
+
+Usuario1= new User(nomUser.toUpperCase(),edadUser);
+
+bandera2=1; 
+
     if ( edadUser >= 18){
-        while (bandera2==1){
-            let opcion= prompt("Ingrese su bebida \n 1) vino: "+bebidas[0]+"\n  2) vodka: "+bebidas[1]+"\n 3) fernet: "+bebidas[2]+"\n 4) gin: "+bebidas[3]+"\n 5) cerveza: "+ bebidas[4]);
-            switch (opcion){
-            
-            case '1': 
-            cantidadBe[0]=prompt("ingrese la cantidad de copas de Vino $" + bebidas[0]+" c/u");
-            if (cantidadBe[0]!=0){
-            carrito.push(bebidas[0]*cantidadBe[0]);
-            }
-            break;
-            
-            case '2': 
-            cantidadBe[1]=prompt("ingrese la cantidad de vasos de Vodka $"+ bebidas[1]+" c/u");
-            if (cantidadBe[1] != 0){
-            carrito.push(bebidas[1]*cantidadBe[1]);
-            }
-            break
-            
-            case '3': 
-            cantidadBe[2]=prompt("ingrese la cantidad de vasos de Vodka $"+ bebidas[2]+" c/u");
-            if (cantidadBe[2] != 0){
-            carrito.push(bebidas[2]*cantidadBe[2]);
-            }
-            break;
-        
-            
-            case '4':
-            cantidadBe[3]=prompt("ingrese la cantidad de vasos de Vodka $"+ bebidas[3]+" c/u");
-            if (cantidadBe[3] != 0){
-            carrito.push(bebidas[3]*cantidadBe[3]);
-            }
-            break;
-        
-            
-            case '5': 
-            cantidadBe[4]=prompt("ingrese la cantidad de vasos de Vodka $"+ bebidas[4]+" c/u");
-            if (cantidadBe[4] != 0){
-            carrito.push(bebidas[4]*cantidadBe[4]);
-            }
-            break;
-            }
-            bandera2 = prompt("¿Quiere seleccionar otra bebida? \n precisione 1 si desea continuar y 0 si desea terminar");
-            if(bandera2== 0){
-            for(var i=0; i<carrito.length; i++){ 
-                calc += carrito[i];
-            }
-            usuarios.push(new User(nomUser.toUpperCase(),edadUser,numUser,cantidadBe,calc));
-            console.log(usuarios); 
-            }
-            else if(bandera2 !=1 && bandera2 !=0){
-            alert("Error");
-            }
-        }
+        mostrar;
     }
-    
+
     else{
         alert("Hola " + nomUser + " te informamos que no cumples con la edad minima requerida");
     }
-    bandera1 = prompt("Desea ingresar un nuevo pedido \n 1 en caso de querer agregar \n 0 en caso de no querer agregar mas")
-    if (bandera1 == 0){
-        bandera=0;
-    }
-    }
-}
+
+    function mostrar(articulo, index) {
+        let verArticulo = ''
+        articulo.forEach((el, index) => {
+            verArticulo += (index + 1) + "-" + "Nombre: " + el.descripcion + ' - ' + "Precio: " + el.precio + "\n"
+        })
+        console.log(verArticulo)
+        return verArticulo
+    } 
+    
+    alert("Los productos cargados Son:\n" + mostrar(articulo))
+        
+        
+
+
+
 
 
 
